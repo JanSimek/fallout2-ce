ceb576e2c451fd1da52979eed77c96cb1dc64368
Jan Šimek <jsimek.cz@gmail.com>
2024-06-03 22:39:57 +0200

    Sfall: add support for loading mods / mod_order.txt

--- src/game.cc
+++ src/game.cc
@@ -1,5 +1,6 @@
 #include "game.h"
 
+#include <algorithm>
 #include <stdio.h>
 #include <string.h>
 
@@ -1376,6 +1377,70 @@ static int gameDbInit()
         if (compat_access(filename, 0) == 0) {
             dbOpen(filename, 0, nullptr, 1);
         }
+    }
+
+    // SFALL: additional mods from the mods directory / mods_order.txt
+    const char* mods_path = "mods";
+    const char* load_order_filename = "mods_order.txt";
+
+    char load_order_filepath[COMPAT_MAX_PATH];
+    compat_makepath(load_order_filepath, nullptr, mods_path, load_order_filename, nullptr);
+
+    // If the mods folder does not exist, create it.
+    compat_mkdir(mods_path);
+
+    // If load order file does not exist, initialize it automatically with mods already in the mods folder.
+    File* stream = fileOpen(load_order_filepath, "r");
+    if (stream == nullptr) {
+        debugPrint("Generating Mods Order file based on the contents of Mods folder: %s\n", load_order_filepath);
+
+        stream = fileOpen(load_order_filepath, "wt");
+        if (stream != nullptr) {
+            char** fileList;
+            int fileListLength = fileNameListInit("mods\\*.dat", &fileList, 0, 0);
+
+            for (int index = 0; index < fileListLength; index++) {
+                fileWriteString(fileList[index], stream);
+                fileWriteString("\n", stream);
+            }
+            fileClose(stream);
+            fileNameListFree(&fileList, 0);
+        }
+    }
+
+    // Add mods from load order file.
+    stream = fileOpen(load_order_filepath, "r");
+    if (stream != nullptr) {
+        char mod[COMPAT_MAX_PATH];
+        while (fileReadString(mod, COMPAT_MAX_PATH, stream)) {
+            char mod_path[COMPAT_MAX_PATH];
+            compat_makepath(mod_path, nullptr, mods_path, mod, nullptr);
+            std::string normalized_mod_path { mod_path };
+
+            if (normalized_mod_path.find_first_of(";#") != std::string::npos)
+                continue; // skip comments
+
+            // ltrim
+            normalized_mod_path.erase(normalized_mod_path.begin(), std::find_if(normalized_mod_path.begin(), normalized_mod_path.end(), [](unsigned char ch) {
+                return !std::isspace(ch);
+            }));
+
+            // rtrim
+            normalized_mod_path.erase(std::find_if(normalized_mod_path.rbegin(), normalized_mod_path.rend(), [](unsigned char ch) {
+                return !std::isspace(ch);
+            }).base(),
+                normalized_mod_path.end());
+
+            if (compat_access(normalized_mod_path.c_str(), 0) == 0) {
+                debugPrint("Loading mod %s\n", normalized_mod_path.c_str());
+                dbOpen(normalized_mod_path.c_str(), 0, nullptr, 1);
+            } else {
+                debugPrint("Skipping invalid mod entry %s in %s\n", normalized_mod_path.c_str(), load_order_filepath);
+            }
+        }
+        fileClose(stream);
+    } else {
+        debugPrint("Error opening %s for read\n", load_order_filepath);
     }
 
     if (compat_access("f2_res.dat", 0) == 0) {
